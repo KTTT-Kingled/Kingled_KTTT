90dac98098c2320cedb0e23e212f3d51ec272ef6
NhaTrucNh <linhlonelikute321@gmail.com>
2022-01-09 14:14:40 +0700

    change UI confirm

--- screens/component/activity/ConfirmCart.js
+++ screens/component/activity/ConfirmCart.js
@@ -1,10 +1,10 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 import { OrderContext } from '../../../contexts/orderContext.js';
 import HeaderComponent from '../headerComponent.js';
 import CircleBackground from './ProgressLine/CircleBackground.js';
 import Line from './ProgressLine/Line.js';
-import LineCart from './ProgressLine/LineCart.js';
 
 const ConfirmCart = ({navigation, route}) => {
 
@@ -19,13 +19,13 @@ const ConfirmCart = ({navigation, route}) => {
   return (
     <ScrollView>
       <HeaderComponent />
-      <View style={styles.container}>
-        <View style={{alignItems: 'center', marginTop: 8, marginBottom: 8}}>
-          <Text style={{color: '#425C59', fontSize: 24}}>
-            THÔNG TIN NHẬN HÀNG
-          </Text>
-        </View>
-        <View style={styles.progress}>
+      <View style={styles.titleTab}>
+        <Icon name="cart-outline" color="#f0ad00" size={22} style={{left: 0}} />
+        <Text style={{color: '#363636', fontSize: 16, marginLeft: 10}}>
+          Thông tin nhận hàng
+        </Text>
+      </View>
+      <View style={styles.progress}>
           <View style={styles.colProgress}>
             <CircleBackground />
             <Text style={{color: '#4cb71a', fontSize: 12, textAlign: 'center'}}>
@@ -54,9 +54,8 @@ const ConfirmCart = ({navigation, route}) => {
             </Text>
           </View>
         </View>
+      <View style={styles.container}>
         <View style={styles.containerList}>
-          <LineCart />
-
           <Text
             style={{
               color: '#417a76',
@@ -68,13 +67,13 @@ const ConfirmCart = ({navigation, route}) => {
             Đơn hàng với mã đơn hàng: {id} của bạn đã được tạo thành công
           </Text>
           <Text style={styles.textConfirm}>
-            Thông tin đơn hàng đã được gửi tới công ty
+            Thông tin đơn hàng đã được gửi tới công ty.
           </Text>
           <Text style={styles.textConfirm}>
-            Chúng tôi sẽ sớm liên hệ đến bạn
+            Chúng tôi sẽ sớm liên hệ đến bạn.
           </Text>
           <Text style={styles.textConfirm}>
-            Cảm ơn bạn đã quan tâm và mua hàng của chúng tôi
+            Cảm ơn bạn đã quan tâm và mua hàng của chúng tôi.
           </Text>
           {/* <Text style={{color:"#FF792E",marginBottom:12,fontSize:18,marginTop:12}}>Sản phẩm của tôi</Text> */}
           {/* <SectionListAbate/> */}
@@ -87,12 +86,14 @@ const ConfirmCart = ({navigation, route}) => {
               width: '100%',
               alignItems: 'center',
               borderRadius: 2,
-              marginTop: 300,
+              marginTop: 50,
               marginBottom: 12,
+              borderRadius: 4,
+              fontSize: 15,
             }}
             onPress={() => DeleteAll()}>
             <Text style={{color: '#FFFFFF', fontSize: 18}}>
-              TIẾP TỤC MUA HÀNG
+              Tiếp tục mua hàng
             </Text>
           </Pressable>
         </View>
@@ -105,16 +106,23 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     backgroundColor: 'white',
-    paddingBottom: 500,
+    height: "100%",
+    backgroundColor: 'white',
     paddingTop: 10,
+    paddingBottom: 190,
   },
   progress: {
-    flex: 1,
+    // flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'flex-start',
-    width: 65,
+    //width: 65,
+    backgroundColor: 'white',
+    padding: 30,
+    marginBottom: 4,
+    borderRadius: 4,
   },
+
   containerList: {
     width: '100%',
     padding: 16,
@@ -128,6 +136,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     //marginRight: -10,
     //marginLeft: -10,
+  },
+  titleTab: {
+    backgroundColor: 'white',
+    padding: 15,
+    height: 55,
+    borderBottomWidth: 0.2,
+    borderBottomColor: '#b0b0b0',
+    borderRadius: 4,
+    marginBottom: 3,
+    flexDirection: 'row',
   },
 });
 export default ConfirmCart;
